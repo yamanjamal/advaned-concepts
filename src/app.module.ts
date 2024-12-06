ec7ffb5a88c4de49b6ad99fa26ed7883fbe31224
yaman jamal <yamanjamal09@gmail.com>
2024-12-06 19:50:37 +0300

    playing around with dynamic modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FibonacciModule } from './fibonacci/fibonacci.module';
 import { CoffiesModule } from './coffies/coffies.module';
+import { HttpClientModule } from './http-client/http-client.module';
 
 @Module({
-  imports: [FibonacciModule, CoffiesModule],
+  imports: [
+    FibonacciModule,
+    CoffiesModule,
+    HttpClientModule.register({ baseUrl: 'http://nestjs.com' }),
+    HttpClientModule.register({ baseUrl: 'http://nestjs.com' }),
+    HttpClientModule.register({ baseUrl: 'http://nestjs.com' }),
+    HttpClientModule.register({ baseUrl: 'http://nestjs.com' }),
+    //alternativly
+    // HttpClientModule.registerAsync({
+    //   useFactory: () => ({ baseUrl: 'http://nestjs.com' }),
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
